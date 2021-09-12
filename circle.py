24671db474c5bf49b143da4b28a011f299506814
Neba Nfonsang <neba.nfonsang@du.edu>
2021-09-11 18:27:34 -0600

    first commit - area function

---
+++ circle.py
@@ -0,0 +1,13 @@
+# Developer A
+## Create a function that computes the area of a circle
+def circle_area(radius):
+    pi = 3.14
+    area = pi*(radius)**2
+    return area
+
+print("Testing the area function:")
+
+print("The area of circle with radius=10 is: ", circle_area(10))
+
+# Developer B
+## Create a function that computes the circumference of a circle 
