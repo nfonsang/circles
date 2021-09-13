ef87319251f154f11b9bbe4de133e382e34bac62
Neba Nfonsang <neba.nfonsang@du.edu>
2021-09-13 15:52:06 -0600

    adding a test for the area function

--- circle.py
+++ circle.py
@@ -10,5 +10,10 @@ print("Testing the area function:")
 print("The area of a circle with radius=10 is: ", circle_area(10))
 print("The area of a circle with radius=15 is: ", circle_area(15))
 
+# Develope a test function for the circle_area function
+def test():
+    assert circle_area(20)==1256
+print(test()) 
+
 # Developer B
 ## Create a function that computes the circumference of a circle 
